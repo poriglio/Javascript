1ba37b88bc09ca21cc604b635c379effb4b475ac
Paula Origlio <minplynn@aol.com>
2015-09-07 12:27:27 -0600

    finished loop practice

---
+++ jsweek2practice.js
@@ -0,0 +1,70 @@
+// LOOP PRACTICE PROBLEM 1
+
+// var total = 0;
+
+// for (var x = 0; x < 6; x++){
+//     console.log(total);
+//     total += 100;
+// };
+
+// LOOP PRACTICE PROBLEM 2
+
+// var total = 1;
+
+// for (var x = 0; x < 7; x++){
+// 	console.log(total);
+// 	total = 2 * total;
+// };
+
+// LOOP PRACTICE PROBLEM 3
+
+// var output = 1;
+
+// for(var x = 0; x < 9; x++){
+// 	if (x < 3){
+// 		console.log(output);
+// 	}
+// 	else if (x >= 6){
+// 		console.log(output+2);
+// 	}
+// 	else{
+// 		console.log(output+1);
+// 	}
+// }
+
+// LOOP PRACTICE PROBLEM 4
+
+// var output = 0;
+
+// for(var x = 0; x < 6; x++){
+// 	console.log(output);
+// 	output+=2;
+// }
+
+// LOOP PRACTICE PROBLEM 5
+
+// var x
+
+// for ( x = 3; x <=15; x+=3 ){
+// 	console.log(x);
+// };
+
+// LOOP PRACTICE PROBLEM 6
+
+// var x
+
+// for ( x = 9; x >= 0; x--){
+// 	console.log(x)
+// }
+
+// LOOP PRACTICE PROBLEM 7
+
+// var x
+// var counter;
+
+// for ( counter = 0; counter < 3; counter++){
+// 	for ( x = 0; x < 4; x++){
+// 		console.log(x);
+// 	};
+// }
+
