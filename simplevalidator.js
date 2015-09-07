8a3407f6c1e3f823515493f80f426866ce813bd1
Paula Origlio <minplynn@aol.com>
2015-09-07 17:31:25 -0600

    started simple validator

---
+++ simplevalidator.js
@@ -0,0 +1,33 @@
+// var userNumber = prompt("Can I get yo digits?\nLike this: 303-269-1573");
+
+// if( ( userNumber.charAt(3) !== "-") || (userNumber.charAt(7) !== "-")){
+// 	alert("I can't chat to that.")
+// }
+// else{
+// 	alert("Great! I'll call during, ah, 'business hours.' ;-)")
+// }
+
+// var userBirthday = prompt("Not that I'm stalking you or anything\nbut when were you born?\nenter it like so: 08/28/88");
+
+// if ( ( userBirthday.charAt(2) !== "/" ) || ( userBirthday.charAt(5) !== "/" ) ) {
+// 	alert("Try that again. Like so: 08/28/19")
+// }
+// else{
+// 	alert("Looks good! I'll have a gift ready.")
+// }
+
+// var zip = prompt ("What's your zip code, newfound lover?");
+
+// if ( ( zip.length === 5 ) && ( !isNaN(zip) ) ){
+// 	alert("true!")
+// }
+// else if ( ( zip.length === 10) && (zip.charAt(5) === "-") && ( !isNan(zip.substring(0,5) ) && ( !isNan(zip.substring(6,10) ) ) ) ){
+// 	alert("true!")
+// }
+// else{
+// 	alert("false!")
+// }
+
+var x = "30932-2398"
+
+console.log(isNaN(x.charAt(6)))
