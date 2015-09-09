8effc91ecf082dac57a49b07840b7b47b2b7862f
Paula Origlio <minplynn@aol.com>
2015-09-09 08:42:24 -0600

    simple validator fully functional

--- simplevalidator.js
+++ simplevalidator.js
@@ -1,91 +1,15 @@
-// var userNumber = prompt("Can I get yo digits?\nLike this: 303-269-1573");
-
-// if( ( userNumber.charAt(3) !== "-") || (userNumber.charAt(7) !== "-")){
-// 	alert("I can't chat to that.")
-// }
-// else{
-// 	alert("Great! I'll call during, ah, 'business hours.' ;-)")
-// }
-
-// var userBirthday = prompt("Not that I'm stalking you or anything\nbut when were you born?\nenter it like so: 08/28/88");
-
-// if ( ( userBirthday.charAt(2) !== "/" ) || ( userBirthday.charAt(5) !== "/" ) ) {
-// 	alert("Try that again. Like so: 08/28/19")
-// }
-// else{
-// 	alert("Looks good! I'll have a gift ready.")
-// }
-
-// var zip = prompt ("What's your zip code, newfound lover?");
-
-// if ( ( zip.length === 5 ) && ( !isNaN(zip) ) ){
-// 	alert("true!")
-// }
-// else if ( ( zip.length === 10) && (zip.charAt(5) === "-") && ( !isNan(zip.substring(0,5) ) && ( !isNan(zip.substring(6,10) ) ) ) ){
-// 	alert("true!")
-// }
-// else{
-// 	alert("false!")
-// }
-
-// var x = "30932-2398"
-
-// console.log(isNaN(x.charAt(6)))
-
 var phone = ""
 var birthday = ""
 var zip = ""
 var state = ""
 var married = ""
 
-// do{phone=prompt("What is your phone number?\nEnter it in this format:\n000-000-0000")}while( ( phone.length!== 12 ) || ( phone.charAt( 3 ) !== "-" ) || (phone.charAt ( 7 ) !== "-" ) || ( isNaN(phone.substring(0,3)) ) || ( isNaN(phone.substring(4,7)) ) || ( isNaN(phone.substring(8,12)) ) )
+do{phone=prompt("What is your phone number?\nEnter it in this format:\n000-000-0000")}while( ( phone.length!== 12 ) || ( phone.charAt( 3 ) !== "-" ) || (phone.charAt ( 7 ) !== "-" ) || ( isNaN(phone.substring(0,3)) ) || ( isNaN(phone.substring(4,7)) ) || ( isNaN(phone.substring(8,12)) ) )
 
-// do{birthday=prompt("When were you born?\nUse the format:\n00/00/00")}while( ( birthday.length !== 8) || ( birthday.charAt(2) !== "/") || (birthday.charAt(5) !== "/") ||  ( isNaN(birthday.substring(0,2)) ) ||  ( isNaN(birthday.substring(3,5)) ) ||  ( isNaN(birthday.substring(6,8)) ) )
+do{birthday=prompt("When were you born?\nUse the format:\n00/00/00")}while( ( birthday.length !== 8) || ( birthday.charAt(2) !== "/") || (birthday.charAt(5) !== "/") ||  ( isNaN(birthday.substring(0,2)) ) ||  ( isNaN(birthday.substring(3,5)) ) ||  ( isNaN(birthday.substring(6,8)) ) )
 
+do{zip=prompt("What is your zip code?\nUse one of the following formats:\n00000 or 00000-0000")}while(((zip.length!==10)&&(zip.length!==5))||((zip.length===5)&&(isNaN(zip)))||((zip.length===10)&&((isNaN(zip.substring(0,5))||(isNaN(zip.substring(6,10))||(zip.charAt(5)!=="-"))))))
 
+do{state = prompt("Where do you live?\nGive me your two-letter state abbreviation.");var letters = (/^[0-9a-zA-Z]+$/)}while((state.length!==2)||(!isNaN(state))||(!isNaN(state.substring(0,1))||!(state.match(letters))||(!isNaN(state.substring(1,2)))))
 
-// COME BACK TO THE ZIP VALIDATION
-
-// do{zip=prompt("What is your zip code?\nUse one of the following formats:\n00000 or 00000-0000")}while(
-// // ((zip.length===5) && (isNaN(zip))) 
-// 				// ORR
-// (
-
-// 	(zip.length===10) 
-// 	&& 
-// (
-// 	(isNaN(zip.substring(0,5))) || (isNaN(zip.substring(6,10)))
-// 		)
-
-// 	)
-// )
-
-// COME BACK TO THE ZIP VALIDATION
-
-
-
-
-
-// STATE VALIDATION
-// STILL NEED TO MAKE SURE IT WON'T EVALUATE TRUE FOR NON-LETTER CHARACTERS OTHER THAN NUMBERS
-
-do{state = prompt("Where do you live?\nGive me your two-letter state abbreviation.")}while((state.length!==2)||(!isNaN(state)) )
-
-state = state.toUpperCase();
-
-console.log(state);
-
-
-
-
-//MARRIAGE
-
-married = prompt ( 'Are you married?');
-
-do{prompt('Are you married? Answer yes or no.')}while( !toLowerCase(married) )
-
-console.log(typeof married)
-
-console.log(married);
-
-
+do{married=prompt("Are you married?\nEnter yes or no.");married=married.toUpperCase();}while((married!=="YES")&&(married!=="NO"));
