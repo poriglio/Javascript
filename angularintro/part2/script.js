a770bb618785cf9a50b6ac20ca9d158fd9a8ff22
Paula Origlio <minplynn@aol.com>
2015-09-10 12:28:58 -0600

    part of part 2 complete

--- angularintro/part2/script.js
+++ angularintro/part2/script.js
@@ -2,4 +2,19 @@ angular.module("dogModule", []);
 
 angular.module("dogModule").controller("dogController", ["$scope", function($scope){
 	
+	$scope.hoverColor = function ($event) {
+
+		$scope.colorChange = "magic";
+
+	}
+
+	$scope.exitHoverColor = function ($event) {
+
+		$scope.colorChange = "normal";
+
+	}
+
+
+
+
 }]);
